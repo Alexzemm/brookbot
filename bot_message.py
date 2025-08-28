af6a6e15c64450ef661358b039c261bae0d09e05
Alexzemm <45419160+Alexzemm@users.noreply.github.com>
2025-08-28 15:24:05 +0530

    Update bot_message.py

--- bot_message.py
+++ bot_message.py
@@ -2,6 +2,7 @@ from google import genai
 import os
 
 google_token = os.getenv("GOOGLE_TOKEN")
+
 def message_bot(message, chunk_size=1000):
     client = genai.Client(api_key=google_token)
 
@@ -15,4 +16,5 @@ def message_bot(message, chunk_size=1000):
     messages = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
 
     return messages
+
 
